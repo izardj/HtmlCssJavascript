4d1ca83cd7d6b4d10c458618722cd3bea77e016f
stagiaire132 <test@test.com>
2017-04-05 10:48:59 +0200

    pause matin

---
+++ WebContent/js/script2.js
@@ -0,0 +1,12 @@
+var a = "Bonjour";
+var b = 12;
+var c = true;
+var d = null;
+var e = 15.2;
+
+alert("Variale a = " + a + " de type " + typeof (a) +
+	  "\nVariale b = " + b + " de type " + typeof (b) +
+	  "\nVariale c = " + c + " de type " + typeof (c) +
+	  "\nVariale d = " + d + " de type " + typeof (d) +
+	  "\nVariale e = " + e + " de type " + typeof (e)
+	  );
---
+++ WebContent/js/script3.js
@@ -0,0 +1,13 @@
+var x = 5, y = 10;
+var add = x+y;
+var sous = x-y;
+var mult = x*y;
+var div = y/x;
+
+alert(  "x = " + x +
+		"\ny = " + y +
+		"\nadditon = " + add +
+		"\nsoustraction = " + sous +
+		"\nmultiplication = " + mult +
+		"\ndivision = " + div	
+		  );
